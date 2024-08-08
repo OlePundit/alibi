38c15031be9fbd3b1032b5ce5f15b91f1e703988
OlePundit <glennomondi@yahoo.com>
2024-08-08 15:17:17 +0300

    product table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,12 +11,16 @@ class Product extends Model
 
     protected $guarded = [];
 
+    protected $casts = [
+        'product_id' => 'array', // Assuming 'product_id' is a JSON array
+    ];
+    
     public function user()
     {
         return $this->belongsTo(User::class);
     }
     public function orders()
     {
-        return $this->belongsToMany(Order::class);
+        return $this->belongsToMany(Order::class, 'order_product', 'product_id', 'order_id');
     }
 }
